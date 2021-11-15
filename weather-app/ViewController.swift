02f470455c84ee8bbfb3fcfbd3ec5358a109508b
manigorman <immanakov@gmail.com>
2021-11-15 19:35:03 +0300

    first commit

---
+++ weather-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather-app
+//
+//  Created by Igor Manakov on 15.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
