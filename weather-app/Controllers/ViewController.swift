74e443831b5c74ee143156335a567a98b248a423
manigorman <immanakov@gmail.com>
2021-11-16 22:08:35 +0300

    added search by device location

--- weather-app/Controllers/ViewController.swift
+++ weather-app/Controllers/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
@@ -16,25 +17,60 @@ class ViewController: UIViewController {
     
     var networkWeatherManager = NetworkWeatherManager()
     
+    lazy var locationManager: CLLocationManager = {
+        let lm = CLLocationManager()
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyKilometer
+        lm.requestWhenInUseAuthorization()
+        return lm
+    }()
+    
     @IBAction func searchPressed(_ sender: UIButton) {
-        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { city in
-            self.networkWeatherManager.fetchCurrentWeather(forCity: city)
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { [unowned self] city in
+            self.networkWeatherManager.fetchCurrentWeather(forRequestType: .cityName(city: city))
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        networkWeatherManager.onCompletion = { CurrentWeather in
-//            print(CurrentWeather.cityName)
-//        }
-        networkWeatherManager.delegate = self
-        networkWeatherManager.fetchCurrentWeather(forCity: "London")
+        networkWeatherManager.onCompletion = { [weak self] currentWeather in
+            guard let self = self else { return }
+            self.updateInterface(weather: currentWeather)
+        }
+//        networkWeatherManager.delegate = self
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func updateInterface(weather: CurrentWeather) {
+        DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString + " °C"
+            self.feelsLikeTemperatureLabel.text = "Feels like " + weather.feelsLikeTemperatureString + " °C"
+            self.weatherIconImageView.image = UIImage(systemName: weather.systemIconNameString)
+        }
     }
 }
 
-extension ViewController: NetworkWeatherManagerDelegate {
-    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather) {
-        print(currentWeather.cityName)
+//extension ViewController: NetworkWeatherManagerDelegate {
+//    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather) {
+//        print(currentWeather.cityName)
+//    }
+//}
+
+// MARK: - CLLocationManagerDelegate
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        
+        networkWeatherManager.fetchCurrentWeather(forRequestType: .coordinate(latitude: latitude, longitude: longitude))
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
     }
 }
-
--- weather-app/Models/NetworkWeatherManager.swift
+++ weather-app/Models/NetworkWeatherManager.swift
@@ -6,26 +6,53 @@
 //
 
 import Foundation
+import CoreLocation
 
-protocol NetworkWeatherManagerDelegate: AnyObject {
-    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather)
-}
+//protocol NetworkWeatherManagerDelegate: AnyObject {
+//    func updateInterface(_: NetworkWeatherManager, with currentWeather: CurrentWeather)
+//}
 
 class NetworkWeatherManager {
     
-    weak var delegate: NetworkWeatherManagerDelegate?
+//    weak var delegate: NetworkWeatherManagerDelegate?
     
-//    var onCompletion: ((CurrentWeather) -> Void)?
+    enum RequestType {
+        case cityName(city: String)
+        case coordinate(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
+    }
     
-    func fetchCurrentWeather(forCity city: String) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&APPID=\(apiKey)"
+    var onCompletion: ((CurrentWeather) -> Void)?
+    
+    func fetchCurrentWeather(forRequestType requestType: RequestType) {
+        var urlString = ""
+        switch requestType {
+        case .cityName(let city):
+            urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&APPID=\(apiKey)&units=metric"
+        case .coordinate(let latitude, let longitude):
+            urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&APPID=\(apiKey)&units=metric"
+        }
+        
+        performRequest(withURLString: urlString)
+    }
+    
+//    func fetchCurrentWeather(forCity city: String) {
+//        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&APPID=\(apiKey)&units=metric"
+//        performRequest(withURLString: urlString)
+//    }
+//
+//    func fetchCurrentWeather(forLatitude latitude: CLLocationDegrees, forLongitute longitute: CLLocationDegrees) {
+//        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitute)&APPID=\(apiKey)&units=metric"
+//        performRequest(withURLString: urlString)
+//    }
+    
+    fileprivate func performRequest(withURLString urlString: String) {
         guard let url = URL(string: urlString) else {return}
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
                 if let currentWeather = self.parseJSON(withData: data) {
-//                    self.onCompletion?(currentWeather)
-                    self.delegate?.updateInterface(self, with: currentWeather)
+                    self.onCompletion?(currentWeather)
+//                    self.delegate?.updateInterface(self, with: currentWeather)
                 }
                 
             }
@@ -33,7 +60,7 @@ class NetworkWeatherManager {
         task.resume()
     }
     
-    func parseJSON(withData data: Data) -> CurrentWeather? {
+    fileprivate func parseJSON(withData data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         do {
             let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
